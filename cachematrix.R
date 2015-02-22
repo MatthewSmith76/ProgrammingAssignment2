38983f11d9983fb6bd8d0220aee8181c6deb3bf9
MatthewSmith76 <mathhew.anton.smith@gmail.com>
2015-02-22 14:48:10 +1100

    First Commit. Results ok on test matrix
    Results ok on test matrix.
    Comments are very sparse.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+# * set the value of the matrix 
+# * get the value of the matrix 
+# * set the value of the matrix inverse
+# * get the value of the matrix inverse
 makeCacheMatrix <- function(x = matrix()) {
 
+  matInv <- NULL
+  set <- function(y) {
+    mat    <<- y
+    matInv <<- NULL
+  }
+  get    <- function() x
+  setInv <- function(iMat) matInv <<- iMat
+  getInv <- function() matInv
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: Computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+# If the inverse has already been calculated 
+# (and the matrix has not changed), then cacheSolve should retrieve the inverse
+# from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  matInv <- x$getInv()
+  if(!is.null(matInv)) {
+    message("getting cached data")
+    return(matInv)
+  }
+  data   <- x$get()
+  matInv <- solve(data)
+  x$setInv(matInv)
+  matInv  
 }
